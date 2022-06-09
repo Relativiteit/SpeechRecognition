623f4d3c0406b97d8529f2033fa7f5ea9016554f
Relativiteit <alejocain@gmail.com>
2022-06-09 22:18:45 +0200

    start

---
+++ wave_example.py
@@ -0,0 +1,16 @@
+# Audio files formats
+# .mp3
+# .flac
+# .wav
+import wave 
+
+# Audio signal parameter
+# - number of channels
+# - sample width 
+# - framerate/sample-rate : 44,100 Hz
+# - number of frames
+# - values of a frame 
+
+obj = wave.open("patrick.wav", "rb")
+
+print("Number of channels", objge)
